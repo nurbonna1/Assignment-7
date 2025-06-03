1f4de0467027e7a1410fd4b4abc9a72ad9ebcdbe
nurbonna1 <nurashekyn.bonna@student.adelaide.edu.au>
2025-06-03 20:48:18 +0930

    Update Token.py

--- Token.py
+++ Token.py
@@ -3,5 +3,11 @@ class Token:
         self.type = type_
         self.value = value
 
+    def getType(self):      # ✅ Required by autograder
+        return self.type
+
+    def getValue(self):     # ✅ Required by autograder
+        return self.value
+
     def __repr__(self):
         return f"Token({self.type}, {self.value})"
